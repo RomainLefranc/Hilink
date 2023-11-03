bc19fb7f84372df02738f4a2c0c5e8040874365d
RomainLefranc <lefranc46@gmail.com>
2023-11-03 12:42:43 +0400

    added GetApp section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Camp from "@/src/components/Camp";
 import Hero from "@/src/components/Hero";
 import Guide from "@/src/components/Guide";
 import Features from "@/src/components/Features";
+import GetApp from "../components/GetApp";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Camp />
       <Guide />
       <Features />
+      <GetApp />
     </>
   );
 }
---
+++ src/components/GetApp.tsx
@@ -0,0 +1,27 @@
+import Button from "@/src/components/Button";
+import appleSvg from "@/public/apple.svg";
+import androidSvg from "@/public/android.svg";
+import phonesSvg from "@/public/phones.png";
+import Image from "next/image";
+function GetApp() {
+  return (
+    <section className="flexCenter w-full flex-col pb-[100px]">
+      <div className="get-app">
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>
+          <p className="regular-16 text-gray-10">Available on iOS and Android</p>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
+            <Button type="button" title="App Store" icon={appleSvg} variant="btn_white" full />
+            <Button type="button" title="Play Store" icon={androidSvg} variant="btn_dark_green_outline" full />
+          </div>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end">
+          <Image src={phonesSvg} alt="phones" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default GetApp;
