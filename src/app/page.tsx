acc26a8a21cd79e1907779d0f63609c02648dd36
RomainLefranc <lefranc46@gmail.com>
2023-11-03 11:24:07 +0400

    added guide section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Camp from "@/src/components/Camp";
 import Hero from "@/src/components/Hero";
+import Guide from "../components/Guide";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Camp />
+      <Guide />
     </>
   );
 }
